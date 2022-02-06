6ff281143c9173606587ed53236061cf68463e14
João Pedro Macedo Oliveira <57810430+joaopmo@users.noreply.github.com>
2022-02-06 01:28:29 +0000

    Commit 6

--- Rental.java
+++ Rental.java
@@ -36,4 +36,13 @@ public class Rental {
         }
         return thisAmount;
    }
+
+   public int getFrequentRenterPoints() { 
+      if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+         this.getDaysRented() > 1){
+           return 2;
+         } else {
+           return 1;
+         }
+   }
 }
